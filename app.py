2628c36e13b9fb87fb935244eeefee56a6ce91cf
LIU_Baoshan <lbs965918542@gmail.com>
2025-05-26 12:18:03 +0800

    Lesson 3 Add index page

--- app.py
+++ app.py
@@ -1,13 +1,36 @@
 #从flask包中导入Flask类， class Flask
-from flask import Flask
+from flask import Flask, render_template
 from markupsafe import escape
 
 #通过实例化class Flask，创建了程序对象app
 app = Flask(__name__)
 
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
+
+
+
+
+
 #注册一个请求处理函数，官方名为 view function.
 #用装饰器app.route()绑定对应的URL。当用户在浏览器访问这个URL时，就会触发这个函数，获取返回值。
-@app.route('/')
+@app.route('/lesson1')
 def hello():
     return 'Welcome to My Watchlist!'
 
